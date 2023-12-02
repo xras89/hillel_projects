175140c62ced9ef73ba346f3ab171caef91aa5a7
Oleksandr Radchenko <xras89@gmail.com>
2023-12-02 11:53:35 +0100

    All parts - Done

--- day 4/day_4_part_2.py
+++ day 4/day_4_part_2.py
@@ -1,10 +1,3 @@
-# number = int(input("Please enter number: "))
-#
-# for i in range(1, number + 1):
-#     k = i ** 2
-#     if k % i == i:
-#         print(i,"*", i, "=", i*i)
-
 number = int(input("Please enter the number: "))
 
 for i in range(1, number + 1):
@@ -13,4 +6,3 @@ for i in range(1, number + 1):
 
     if str(i) == last_digits:
         print(i, "*", i, "=", i*i)
-        
